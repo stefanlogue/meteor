5aa2ec5b5dee57865ca78963afdc77bd8cd96698
Stefan Logue <stefanlogue@icloud.com>
2024-02-06 23:33:23 +0000

    ci(release): only commit changelog

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,7 @@ read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
 
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
 	python scripts/prepare_changelog.py
-	git add -A
+	git add CHANGELOG.md
 	git commit -m "chore: bump version to $TAG for release" || true && git push
 	echo "Creating new git tag $TAG"
 	git tag "$TAG" -m "$TAG"
