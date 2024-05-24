022fcd2bc9d5df4f721f2bd577c0cf4f62ead695
Stefan Logue <stefanlogue@icloud.com>
2024-05-24 17:41:06 +0100

    refactor(prefix): change order of default prefixes

--- pkg/config/prefix.go
+++ pkg/config/prefix.go
@@ -13,24 +13,21 @@ type Prefix struct {
 
 type Prefixes []Prefix
 
-var (
-	DefaultPrefixes = []huh.Option[string]{
-		huh.NewOption("feat - a new feature", "feat"),
-		huh.NewOption("fix - a bug fix", "fix"),
-		huh.NewOption("docs - documentation only changes", "docs"),
-		huh.NewOption("style - changes that do not affect the meaning of the code", "style"),
-		huh.NewOption("refactor - a code change that neither fixes a bug nor adds a feature", "refactor"),
-		huh.NewOption("perf - a code change that improves performance", "perf"),
-		huh.NewOption("test - adding missing tests or correcting existing tests", "test"),
-		huh.NewOption("chore - changes to the build process or auxiliary tools and libraries", "chore"),
-		huh.NewOption("revert - reverts a previous commit", "revert"),
-		huh.NewOption("ci - changes to our CI configuration files and scripts", "ci"),
-		huh.NewOption("build - changes that affect the build system or external dependencies", "build"),
-	}
-)
+var DefaultPrefixes = []huh.Option[string]{
+	huh.NewOption("feat - a new feature", "feat"),
+	huh.NewOption("fix - a bug fix", "fix"),
+	huh.NewOption("build - changes that affect the build system or external dependencies", "build"),
+	huh.NewOption("chore - changes to the build process or auxiliary tools and libraries", "chore"),
+	huh.NewOption("ci - changes to our CI configuration files and scripts", "ci"),
+	huh.NewOption("docs - documentation only changes", "docs"),
+	huh.NewOption("perf - a code change that improves performance", "perf"),
+	huh.NewOption("refactor - a code change that neither fixes a bug nor adds a feature", "refactor"),
+	huh.NewOption("revert - reverts a previous commit", "revert"),
+	huh.NewOption("style - changes that do not affect the meaning of the code", "style"),
+	huh.NewOption("test - adding missing tests or correcting existing tests", "test"),
+}
 
 func (p *Prefixes) Options() []huh.Option[string] {
-
 	prefixes := []Prefix(*p)
 
 	if len(prefixes) == 0 {
