49d7959907d2e598f231a37f93f557c305fff8f5
Stefan Logue <stefanlogue@icloud.com>
2024-12-19 17:29:53 +0000

    ci(release): script update

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-export TAG=$(svu next --strip-prefix)
+export TAG=$(svu next --force-patch-increment --strip-prefix)
 
-read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
+read -p "Creating new release for v$TAG. Do you want to continue? [Y/n] " prompt
 
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
   python scripts/prepare_changelog.py
   git add CHANGELOG.md
   git commit -m "chore: bump version to $TAG for release" || true && git push
   echo "Creating new git tag $TAG"
-  git tag "$TAG" -m "$TAG"
+  git tag "v$TAG" -m "v$TAG"
   git push --tags
 else
   echo "Cancelled"
